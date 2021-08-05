669b08dde0b21ab38a277698042058c7eab8007e
iamjamesuche <ogbarized@yahoo.com>
2021-08-06 00:10:23 +0100

    Created new folder for all the scripts

---
+++ Scripts/iamjamesucheStage_0.R
@@ -0,0 +1,7 @@
+name <- "James Uche"
+email <- "uchej@student.babcock.edu.ng"
+slack_username <- "@iamjamesuche"
+biostack <- "Data Science"
+twitter_username <- "@iamjamesuche"
+hamming_distance <- sum(slack_username != twitter_username)
+cat (name,email,slack_username,biostack,twitter_username,hamming_distance,sep=", ")
