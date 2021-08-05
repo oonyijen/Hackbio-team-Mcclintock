292505a6fbb436fddb3798699f8d7dece9788c49
iamjamesuche <ogbarized@yahoo.com>
2021-08-06 00:54:45 +0100

    Bash Script

---
+++ teammcclintock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/Chinwendu20/Hackbio-team-Mcclintock.git
+
+cd Scripts
+
+RScript iamjamesucheStage_0.R > iaju.txt
+
+cat *.txt > TEAMMCCLINTOCK.csv
+
+rm *.txt
