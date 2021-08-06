2012df7cd8a921c20704880ebabe2a98f2df9110
Chinwendu2o <amaka013@gmail.com>
2021-08-06 18:55:26 +0100

    A script with an output ofmy details

---
+++ Scripts/Ononiwu_Maureen.cs
@@ -0,0 +1,35 @@
+
+
+using System;
+ 
+class GFG {
+     
+static int hammingDist(String str1,
+                       String str2)
+{
+    int i = 0, count = 0;
+    while (i < str1.Length)
+    {
+        if (str1[i] != str2[i])
+            count++;
+        i++;
+    }
+    return count;
+}
+ 
+
+public static void Main ()
+{
+   
+    String Name = "Ononiwu Maureen Chiamaka";
+    String Email = "maureen.ononiwu13@gmail.com";
+    String Slack_username = "@Babe_Chinwendum";
+    String Biostack = "Drug discovery and development";
+    String Twitter_Username="@Babe_ChinwenduIn";
+    String Hamming_Distance= hammingDist(Twitter_Username, Slack_username)
+    
+ 
+   
+    Console.Write(Name+','+Email+','+Slack_Username+','+Biostack+','+Twitter_Username+','+Hamming_Distance);
+}
+}
