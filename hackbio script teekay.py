fca560076a07ffe2a7c503c65ad6ed5436b97374
Tee-cloud <85879005+Tee-cloud@users.noreply.github.com>
2021-08-06 22:57:49 +0100

    Add files via upload

---
+++ hackbio script teekay.py
@@ -0,0 +1,12 @@
+#name 
+#email
+#slack user 
+#biostack
+
+userName = 'Latifah Abdulkarim'
+userMail = 'abdulkarimlatifah8@gmail.com'
+userSlack = '@Latifah'
+userTwitter = '@LateefahKareem'
+userBiostack = 'Data analytics and Vaccine informatics'
+
+print(f"Hello there, \nMy name is {userName} \nYou can reach via mail @ {userMail} \n\t Via slack at {userSlack} and via twitter at {userTwitter} \n My Biostack is {userBiostack}. \nThank you ")
