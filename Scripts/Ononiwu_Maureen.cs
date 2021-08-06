bec7d2a4138453e7ab4a15c50c21a32631ecd831
Chinwendu20 <amaka013@gmail.com>
2021-08-06 19:37:44 +0000

    Compiled my code

--- Scripts/Ononiwu_Maureen.cs
+++ Scripts/Ononiwu_Maureen.cs
@@ -25,11 +25,11 @@ public static void Main ()
     String Email = "maureen.ononiwu13@gmail.com";
     String Slack_username = "@Babe_Chinwendum";
     String Biostack = "Drug discovery and development";
-    String Twitter_Username="@Babe_ChinwenduIn";
-    String Hamming_Distance= hammingDist(Twitter_Username, Slack_username)
+    String Twitter_Username="@Babe_ChinwendIn";
+    int Hamming_Distance= hammingDist(Twitter_Username, Slack_username);
     
  
    
-    Console.Write(Name+','+Email+','+Slack_Username+','+Biostack+','+Twitter_Username+','+Hamming_Distance);
+    Console.Write(Name+','+Email+','+Slack_username+','+Biostack+','+Twitter_Username+','+Hamming_Distance);
 }
 }
