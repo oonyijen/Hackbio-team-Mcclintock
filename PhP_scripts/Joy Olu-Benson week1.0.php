998e65cb5304ebd3b7d2731e5bf32e6afa547360
Chinwendu20 <amaka013@gmail.com>
2021-08-07 07:54:12 +0000

    Made changes

--- PhP_scripts/Joy Olu-Benson week1.0.php
+++ PhP_scripts/Joy Olu-Benson week1.0.php
@@ -5,25 +5,15 @@
  define("Twitter Handle", "@BENSON");
  $U="Hamming Distance";
  $Name= "Joy"; $Slack_Username="@Benson,";
-        $userNames = ["Joy Olu-Benson,",];
-      $userMails = ["joyolubenson@yahoo.com,",];
-    $userSlkname = ["@Benson,",];
- $bioStack = ["Medicinal Chemistry,",];
-    $twitHandle = ["@JAD_BENSON,",];
+        $userNames = ["Joy Olu-Benson",];
+      $userMails = ["joyolubenson@yahoo.com",];
+    $userSlkname = ["@Benson",];
+ $bioStack = ["Medicinal Chemistry",];
+    $twitHandle = ["@JAD_BENSON",];
  $hamLength =[strlen("@BENSON")-strlen("@Benson"),];
  //Basically I have defined, arrayed all inputs and evaluted hamming lenth indirectly by manipulating string lengths
  //Next is to define the table
- echo"
- <tr><b>
-    <td>".$userNames[0]."</td>
-    <td>","</td>
-               <td>".$userMails[0]."</td>
-             <td>".$userSlkname[0]."</td>
-           <td>".$bioStack[0]."</td>
-        <td>".$twitHandle[0]."</td>
-    <td>".$hamLength[0]."</td>    
- </b>
- </tr>";
-
  
+echo "$userNames[0],$userMails[0],$userSlkname[0],$bioStack[0],$twitHandle[0], $hamLength[0]";
+
 ?>
--- teammcclintock.sh
+++ teammcclintock.sh
@@ -7,39 +7,39 @@ git clone https://github.com/Chinwendu20/Hackbio-team-Mcclintock.git
 cd Hackbio-team-Mcclintock
 
 echo "Name, Email, Slack_Username(with @), Biostack, Twitter_username, Hamming_Distance">TEAMMCCLINTOCK.csv
-
-for file in "R_scripts"
+wdwd
+for file in "$$R_scripts/"
 do
 RScript $file >> TEAMMCCLINTOCK.csv
 done
 
-for file in "Py_scripts"
+for file in "Py_scripts/"
 do
 python3 $file >> TEAMMCCLINTOCK.csv
 done
 
-for file in "PhP_scripts"
+for file in "PhP_scripts/"
 do
 php $file >> TEAMMCCLINTOCK.csv
 done
 
-for file in "perl_scripts"
+for file in "perl_scripts/"
 do
 perl $file >> TEAMMCCLINTOCK.csv
 done
 
-for file in "JS_scripts"
+for file in "JS_scripts/"
 do
 node $file >> TEAMMCCLINTOCK.csv
 done
 
-for file in "C#_scripts"
+for file in "C#_scripts/"
 do
-if ["$file"=="#.cs"]
+if ["$file"=="*.cs"]
 then
 mcs $file
 fi
-if ["$file"=="#.exe"]
+if ["$file"=="*.exe"]
 then
 mono $file >> TEAMMCCLINTOCK.csv
 fi
